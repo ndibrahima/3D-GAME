6f5dfe14f6ef8d5bfcfc6d9ae866fe3da4807f68
ndibrahima <ndiayeibrahim666@gmail.com>
2020-09-30 16:32:20 +0200

    3D_GAME
    3D GAME

---
+++ Assets/AnimateGirl.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer), typeof(Animator))]
+public class AnimateGirl : MonoBehaviour
+{
+    
+    [Tooltip("Vitesse en seconde")]
+    public int MaxSpeed = 2;
+
+    // Autres scripts
+    private SpriteRenderer spriteRenderer;
+     private Vector3 speed;
+    private Animator animator;
+    private static readonly int Speed = Animator.StringToHash("Speed");
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        var maxDisancePerFrame = MaxSpeed * Time.deltaTime;
+        Vector3 move = Vector3.zero;
+        
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            move += Vector3.right * maxDisancePerFrame;
+            spriteRenderer.flipX = false;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            move += Vector3.left * maxDisancePerFrame;
+            spriteRenderer.flipX = true;
+        }
+        
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            move += Vector3.up * maxDisancePerFrame;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            move += Vector3.down * maxDisancePerFrame;
+        }
+
+        animator.SetFloat(Speed, move.magnitude * 10f);
+        Debug.Log(move.magnitude);
+        this.transform.position = this.transform.position + move;
+
+    }
+}
---
+++ Assets/Tracker.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tracker : MonoBehaviour
+{
+    public GameObject Target;
+
+    public Vector2 DeadWindow;
+
+    public float FollowSpeed = 0.02f;
+    private Rect _rect;
+
+    private void OnDrawGizmos()
+    {    
+        Gizmos.DrawWireCube(_rect.center, _rect.size);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        _rect = new Rect((Vector2) transform.position - DeadWindow * 0.5f, DeadWindow);
+        if (!_rect.Contains(Target.transform.position))
+        {
+            var z = transform.position.z;
+            var nextPos = Vector3.Lerp(transform.position, Target.transform.position, 0.02f);
+            nextPos.z = z;
+            transform.position = nextPos;
+        }
+    }
+}
